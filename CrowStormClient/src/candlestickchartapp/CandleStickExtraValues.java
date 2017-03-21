0cc138ce8fce4e6236c0c3db00e2112f2df69baa
Nate Rock <rockhowse@gmail.com>
2017-03-21 12:18:44 -0500

    integreated Oracle's candlestick chart example with CrowStormClient

---
+++ CrowStormClient/src/candlestickchartapp/CandleStickExtraValues.java
@@ -0,0 +1,38 @@
+/* ....Show License.... */
+package candlestickchartapp;
+
+/** Data extra values for storing close, high and low. */
+public class CandleStickExtraValues  {
+    private double close;
+    private double high;
+    private double low;
+    private double average;
+ 
+    public CandleStickExtraValues(double close, double high, double low, double average) {
+        this.close = close;
+        this.high = high;
+        this.low = low;
+        this.average = average;
+    }
+ 
+    public double getClose() {
+        return close;
+    }
+ 
+    public double getHigh() {
+        return high;
+    }
+ 
+    public double getLow() {
+        return low;
+    }
+ 
+    public double getAverage() {
+        return average;
+    }
+ 
+    @Override
+    public String toString() {
+        return "CandleStickExtraValues{" + "close=" + close + ", high=" + high + ", low=" + low + ", average=" + average + '}';
+    }
+}
--- CrowStormClient/src/crowstormclient/CrowStormClient.java
+++ CrowStormClient/src/crowstormclient/CrowStormClient.java
@@ -36,6 +36,12 @@ import javafx.stage.Stage;
 import org.apache.commons.io.IOUtils;
 import org.json.JSONObject;
 
+import candlestickchartapp.CandleStickChart;
+import candlestickchartapp.CandleStickExtraValues;
+import javafx.scene.Parent;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+
 
 /**
  *
@@ -43,11 +49,77 @@ import org.json.JSONObject;
  */
 public class CrowStormClient extends Application {
 
-    public static ObservableList data = 
+    
+    // DAY, OPEN, CLOSE, HIGH, LOW, AVERAGE
+    private static double[][] data2 = new double[][]{
+            {1,  25, 20, 32, 16, 20},
+            {2,  26, 30, 33, 22, 25},
+            {3,  30, 38, 40, 20, 32},
+            {4,  24, 30, 34, 22, 30},
+            {5,  26, 36, 40, 24, 32},
+            {6,  28, 38, 45, 25, 34},
+            {7,  36, 30, 44, 28, 39},
+            {8,  30, 18, 36, 16, 31},
+            {9,  40, 50, 52, 36, 41},
+            {10, 30, 34, 38, 28, 36},
+            {11, 24, 12, 30, 8,  32.4},
+            {12, 28, 40, 46, 25, 31.6},
+            {13, 28, 18, 36, 14, 32.6},
+            {14, 38, 30, 40, 26, 30.6},
+            {15, 28, 33, 40, 28, 30.6},
+            {16, 25, 10, 32, 6,  30.1},
+            {17, 26, 30, 42, 18, 27.3},
+            {18, 20, 18, 30, 10, 21.9},
+            {19, 20, 10, 30, 5,  21.9},
+            {20, 26, 16, 32, 10, 17.9},
+            {21, 38, 40, 44, 32, 18.9},
+            {22, 26, 40, 41, 12, 18.9},
+            {23, 30, 18, 34, 10, 18.9},
+            {24, 12, 23, 26, 12, 18.2},
+            {25, 30, 40, 45, 16, 18.9},
+            {26, 25, 35, 38, 20, 21.4},
+            {27, 24, 12, 30, 8,  19.6},
+            {28, 23, 44, 46, 15, 22.2},
+            {29, 28, 18, 30, 12, 23},
+            {30, 28, 18, 30, 12, 23.2},
+            {31, 28, 18, 30, 12, 22}
+    };
+ 
+    private CandleStickChart chart;
+    private NumberAxis xAxis;
+    private NumberAxis yAxis;   
+    
+   public Parent createContent() {
+        xAxis = new NumberAxis(0,32,1);
+        xAxis.setMinorTickCount(0);
+        yAxis = new NumberAxis();
+        chart = new CandleStickChart(xAxis,yAxis);
+        // setup chart
+        xAxis.setLabel("Day");
+        yAxis.setLabel("Price");
+        // add starting data
+        XYChart.Series<Number,Number> series = new XYChart.Series<Number,Number>();
+        for (int i=0; i< data2.length; i++) {
+            double[] day = data2[i];
+            series.getData().add(
+                new XYChart.Data<Number,Number>(day[0],day[1],new CandleStickExtraValues(day[2],day[3],day[4],day[5]))
+            );
+        }
+        ObservableList<XYChart.Series<Number,Number>> data = chart.getData();
+        if (data == null) {
+            data = FXCollections.observableArrayList(series);
+            chart.setData(data);
+        } else {
+            chart.getData().add(series);
+        }
+        return chart;
+    }    
+    
+    private static ObservableList data = 
         FXCollections.observableArrayList();  
     
-    ListView searchResults;
-    TextField searchTextField;
+    private ListView searchResults;
+    private TextField searchTextField;
              
     public void searchResults() {
         JSONObject json;
@@ -84,8 +156,9 @@ public class CrowStormClient extends Application {
         
         // get the chart componant
         // TODO: get OHLC chart componant working here
+        createContent();
         
-        vbox.getChildren().addAll(searchResults);
+        vbox.getChildren().addAll(searchResults, chart);
         return vbox;
     }
     
